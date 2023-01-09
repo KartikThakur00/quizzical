ca352fbf50f3fdabe92743dfc5a0ad437af9a14f
Kartik Thakur <kartikthakur029@gmail.com>
2023-01-10 00:13:42 +0530

    commit

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,59 +1,181 @@
-// import React from "react";
-// import Start from "./Start";
-// import Qcard from "./Qcard";
-// import { nanoid } from "nanoid";
-
-function Question() {
-  const [apiQuestions, setApiQuestions] = React.useState([]);
-  const [questions, setQuestions] = React.useState([]);
-
-  // getting data from api
+import React from "react";
+import Start from "./Start";
+import { nanoid } from "nanoid";
+import { shuffle, decodeHtml } from "../utils";
+export default function Question(){
+     // this state decide whether quiz shuld be rendered or not
+  const [status, setStatus] = React.useState(false);
+  // this state is used to get and set question
+  const [quizQuestions, setQuizQuestions] = React.useState([]);
+  // this state decide whether a new game started or not
+  const [newGame, setNewGame] = React.useState(false);
+  // check for gameover
+  const [gameOver, setGameOver] = React.useState(false);
+  // track the score
+  const [score, setScore] = React.useState(0);
+  // getting questions from api
   React.useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then((res) => res.json())
-      .then((data) => setApiQuestions(data.results));
-  }, []);
+      .then((data) => setQuizQuestions(generateQuiz(data.results)));
+    // .then(data=>console.log(data))
+  }, [newGame]);
 
-  // console.log(questions.length);
-  console.log(apiQuestions);
+  function toggleStatus() {
+    setStatus((oldStatus) => !oldStatus);
+  }
 
-//   // setting data
-//   function requestQ() {
-//     setQuestions(
-//       apiQuestions.map((Question) => {
-//         const answers = Question.incorrect_answers.map((A) => (decodeHtml(A)));
+  function generateQuiz(apiData) {
+    const quizQuestion = apiData.map((quiz) => {
+      return {
+        id: nanoid(),
+        question: decodeHtml(quiz.question),
+        answers: shuffle(generateAnswer(quiz)),
+      };
+    });
+    return quizQuestion;
+  }
 
-//         return {
-//           id: nanoid(),
-//           question: decodeHtml(Question.question),
-//           incorrectAnswers: answers,
-//           correctAnswer: decodeHtml(Question.correct_answer),
-//         };
-//       })
-//     );
-//   }
+  function generateAnswer(dataFromApi) {
+    const incorrectAnswers = dataFromApi.incorrect_answers;
+    const correctAnswer = dataFromApi.correct_answer;
+    const answers = incorrectAnswers.map((incorrectAnswer) => {
+      return {
+        id: nanoid(),
+        value: incorrectAnswer,
+        isCorrect: false,
+        isChecked: false,
+      };
+    });
+    answers.push({
+      id: nanoid(),
+      value: correctAnswer,
+      isCorrect: true,
+      isChecked: false,
+    });
+    return answers;
+  }
 
-//   const questionCards = questions.map((data) => (
-//     <Qcard
-//       key={data.id}
-//       question={data.question}
-//       answers={data.incorrectAnswers}
-//       correctAnswer={data.correctAnswer}
-//     />
-//   ));
+  function AnswerOptions(props) {
+    const styleBeforeGameOver = props.isChecked ? "#D6DBF5" : "";
 
-//   console.log(questionCards);
+    function styleGameOver() {
+      if (props.isCorrect) {
+        return "#94D7A2";
+      } else if (props.isChecked && !props.isCorrect) {
+        return "#D6DBF5";
+      }
+    }
+
+    return (
+      <div
+        onClick={props.handleAnswerCheck}
+        id={props.id}
+        style={{
+          backgroundColor: props.gameOver
+            ? styleGameOver()
+            : styleBeforeGameOver,
+        }}
+        className="answer"
+      >
+        {props.value}
+      </div>
+    );
+  }
+
+  function handleAnswerCheck(questionId, answerId) {
+    setQuizQuestions((oldQuizQuestions) =>
+      oldQuizQuestions.map((oldQuizQuestion) => {
+        return oldQuizQuestion.id === questionId
+          ? {
+              ...oldQuizQuestion,
+              answers: oldQuizQuestion.answers.map((answer) => {
+                return answer.id === answerId
+                  ? {
+                      ...answer,
+                      isChecked: !answer.isChecked,
+                    }
+                  : { ...answer, isChecked: false };
+              }),
+            }
+          : oldQuizQuestion;
+      })
+    );
+  }
+
+  function checkAnswers() {
+    setGameOver(true);
+    const answerTracker = quizQuestions.map((quizQuestion) => {
+      const answer = quizQuestion.answers;
+      return answer.some((answer) => answer.isChecked && answer.isCorrect);
+    });
+    let count = 0;
+    answerTracker.forEach((answerBlock) =>
+      answerBlock ? (count = count + 1) : count
+    );
+    setScore(count);
+  }
+
+  function handleClick() {
+    !gameOver ? checkAnswers() : playAgain();
+  }
+
+  function playAgain() {
+    setNewGame((newGame) => !newGame);
+    setGameOver((prevGameOver) => !prevGameOver);
+  }
+
+  const Questions = quizQuestions.map((question) => {
+    return (
+      <div className="card">
+        {/* <Question key={question.id} question={question.question} /> */}
+        <h3 className="question" key={question.id}>{question.question}</h3>
+        <div className="answer-container">
+          {question.answers.map((answer) => {
+            return (
+              <AnswerOptions
+                key={answer.id}
+                id={answer.id}
+                value={decodeHtml(answer.value)}
+                isCorrect={answer.isCorrect}
+                isChecked={answer.isChecked}
+                handleAnswerCheck={() =>
+                  handleAnswerCheck(question.id, answer.id)
+                }
+                gameOver={gameOver}
+              />
+            );
+          })}
+        </div>
+        <hr className="line"/>
+      </div>
+    );
+  });
+
+  console.log(quizQuestions);
+  console.log(Questions);
+
   return (
     <>
-      {questions.length > 0 ? (
-        <div className="card-container">
-          {questionCards}
-          <button className="check-button">Check Answers</button>
-        </div>
+      {!status ? (
+        <Start handler={toggleStatus} />
       ) : (
-        <Start handler={requestQ} />
+        <div className="quiz">
+          <div className="quiz-container">
+            {Questions}
+            {gameOver ? (
+              <h2>you got {score}/5 correct!</h2>
+            ) :("")}
+
+
+              <div className="button-container">
+                <button className="btn" onClick={handleClick}>
+                  {!gameOver ? "Check Answer" : "Play Again"}
+                </button>
+              </div>
+          </div>
+        </div>
       )}
     </>
   );
 }
-export default Question;
